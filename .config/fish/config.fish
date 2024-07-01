3788694917c5b868c08e997510d8fb9e06478837
robertjshirts <333221.rob@gmail.com>
2024-07-01 11:11:06 -0600

    impl starship.toml

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,11 +4,11 @@ end
 
 # Funny fish logo at startup
 function fish_greeting
-    fish_logo
+    fish_logo blue cyan green
 end
 
 # Alias fish logo to fl bc we the goats
-alias fl=fish_logo
+alias fl="fish_logo blue cyan green"
 
 # Init starship (prompt) (nvm we goin through a lil thng rn)
 # starship init fish | source
