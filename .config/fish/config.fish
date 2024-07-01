1446e137d0c376f92dc0c2cab229e14b155f31fc
robertjshirts <333221.rob@gmail.com>
2024-07-01 01:10:32 -0600

    impl fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Funny fish logo at startup
+function fish_greeting
+    fish_logo
+end
+
+# Alias fish logo to fl bc we the goats
+alias fl=fish_logo
+
+# Init starship (prompt) (nvm we goin through a lil thng rn)
+# starship init fish | source
+
+# Init zoxide (better cd)
+zoxide init fish | source 
+alias cd=z
+
