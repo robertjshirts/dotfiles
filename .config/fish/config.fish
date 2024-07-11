181ed5b7a029d0df4f70944b56f1024f0a472643
robertjshirts <333221.rob@gmail.com>
2024-07-10 19:10:55 -0600

    add idv4 alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,9 @@ end
 # Alias fish logo to fl bc we the goats
 alias fl="fish_logo blue cyan green"
 
+# Alias idv4 to copy a new uuidv4 to my clipboard
+alias idv4="uuidgen -r | xclip -selection clipboard"
+
 # Init starship (prompt) (nvm we goin through a lil thng rn)
 # starship init fish | source
 
