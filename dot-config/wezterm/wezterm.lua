5f01b9739be897337b1dbf0c4d4269afbd09fce7
robertjshirts <robertjshirts@gmail.com>
2024-12-30 00:02:53 -0700

    update stow names, clean up bashrc

---
+++ dot-config/wezterm/wezterm.lua
@@ -0,0 +1,25 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- Try wayland fixing
+config.enable_wayland = false
+
+-- Use nerd font
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+
+-- Use bash as default shell
+config.default_prog = { "bash" }
+
+-- Use a color scheme
+config.color_scheme = "Ashes (base16)"
+
+-- OPACTICITY
+config.window_background_opacity = 0.925
+
+-- and finally, return the configuration to wezterm
+return config
