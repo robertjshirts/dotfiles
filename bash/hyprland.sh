a509230c7c9b725c105018db892d1c275ff97327
robertjshirts <robertjshirts@gmail.com>
2024-12-31 00:35:22 -0700

    add more cursor settings (i hate xcursor)

--- bash/hyprland.sh
+++ bash/hyprland.sh
@@ -1,4 +1,8 @@
-# Hyprland
+# hyprland.sh
+
+# if a display manager is already running, do not run uwsm or hyprland
+[ -n "$WAYLAND_DISPLAY" ] && return 0
+
 if check_command hyprland; then 
     if check_command uwsm; then
         if uwsm check may-start; then
