2ce4f6c3327b5e0b546a4af3de3f5a5cf6b22692
robertjshirts <robertjshirts@gmail.com>
2025-01-09 21:27:55 -0700

    add lazygit alias

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -2,5 +2,6 @@ alias ls='ls --color=auto -a'             # List hidden files and in color :)
 alias grep='grep --color=auto'
 alias lss='ls --color=auto'               # List files (not including hidden ones)
 alias lzd='lazydocker'                    # Lazydocker
+alias lzg='lazygit'                       # Lazygit
 alias permaban='rm -rf'                   # LMAO
 alias stows='stow -v --dotfiles .'     # Shorthand for stow command
