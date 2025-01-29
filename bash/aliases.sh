ac5e0c8267204b471eb6f2d88d97e5710841508f
robertjshirts <robertjshirts@gmail.com>
2025-01-29 14:11:47 -0700

    update stow alias, add docker-compose alias

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,7 +1,14 @@
-alias ls='ls --color=auto -a'             # List hidden files and in color :)
+alias ls='ls --color=auto -a'                        # List hidden files and in color :)
 alias grep='grep --color=auto'
-alias lss='ls --color=auto'               # List files (not including hidden ones)
-alias lzd='lazydocker'                    # Lazydocker
-alias lzg='lazygit'                       # Lazygit
-alias permaban='rm -rf'                   # LMAO
-alias stows='stow -v --dotfiles .'     # Shorthand for stow command
+alias lss='ls --color=auto'                          # List files (not including hidden ones)
+alias lzd='lazydocker'                               # Lazydocker
+alias lzg='lazygit'                                  # Lazygit
+alias permaban='rm -rf'                              # LMAO
+alias stows='/usr/bin/stow -v --dotfiles ~/dotfiles' # Shorthand for stow command
+alias dc="docker-compose"                            # shorthand for docker DASH compose
+
+# Complicated aliases
+stow() {
+    echo "use 'stows' dumbass"
+    echo "if you really need the original, use /usr/bin/stow"
+}
